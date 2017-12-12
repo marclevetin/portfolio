815bc6103bb1469f3def200f70f1e222124661e5
marclevetin <marc@derbyhat.org>
2017-12-11 21:28:26 -0500

    created placeholder data for PM projects

---
+++ src/data/pmprojects.js
@@ -0,0 +1,9 @@
+const pmprojectdata = [
+  {id: 1, project: 'Company financials', problem: 'TBD', solution: 'TBD', description: 'TBD', category: 'TBD', team:[], company: 'Buildium'},
+  {id: 2, project: 'Cash flow statement', problem: 'TBD', solution: 'TBD', description: 'TBD', category: 'TBD', team:[], company: 'Buildium'},
+  {id: 3, project: 'Automatic bank reconciliation', problem: 'TBD', solution: 'TBD', description: 'TBD', category: 'TBD', team:[],company: 'Buildium'},
+  {id: 4, project: 'Overhaul support ticket system', problem: 'TBD', solution: 'TBD', description: 'TBD', category: 'TBD', team:[],company: 'Buildium'},
+  {id: 5, project: 'Implement stronger passwords', problem: 'TBD', solution: 'TBD', description: 'TBD', category: 'TBD', team:[], company: 'Buildium'},
+]
+
+export default pmprojectdata;
