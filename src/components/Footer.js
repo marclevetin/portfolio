76f334ecd69bd5ccf9be61609a79a9e75414b83f
marclevetin <marc@derbyhat.org>
2017-12-11 22:11:19 -0500

    added data to footer, updated app title, updated header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = (props) => {
     return (
       <div>
-        This is the Footer
+        <p><a href="https://www.linkedin.com/in/marclevetin/" target="_blank">LinkedIn</a> | <a href="https://github.com/marclevetin/" target="_blank">Github</a> | <a href="https://twitter.com/_derbyhat" target="_blank">Twitter</a></p>
+        <p>Copyright 2017 - Marc Levetin</p>
       </div>
     )
 };
